112a2adc348d8f7d441003ecffc1752cad6a2596
Marcus Gill <jmarcusgill@gmail.com>
2017-11-18 11:45:11 -0800

    created app folder with configs, can auth one user

---
+++ bangazon/app/controllers/homeController.js
@@ -0,0 +1,8 @@
+﻿app.controller("homeController", ["$scope", "$http", function ($scope, $http) {
+
+    $http.get("/api/values").then(function (result) {
+        $scope.values = result.data;
+    });
+
+    $scope.message = "Hello World!";
+}]);
